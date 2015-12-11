bbe2e48e7fa3160a9c890843bf3eacfd72ef2460
Graffine Wu <graffine.wu@ileopard.com>
2015-12-11 15:53:32 +0800

    Add formatter method to create data uri

--- parse_data_uri.py
+++ parse_data_uri.py
@@ -1,7 +1,13 @@
 import re
-from base64 import urlsafe_b64decode
+from base64 import urlsafe_b64encode, urlsafe_b64decode
 
-DATA_URI_REGEX = (r'data:' + r'(?P<mimetype>[\w]+\/[\w\-\+\.]+)?' + r'(\;charset\=(?P<charset>)[\w\-\+\.]+)?' + r'(\;(?P<base64>base64))?' + r',(?P<data>.*)')
+MIMETYPE_REGEX_PATTERN = (r'(?P<mimetype>[\w]+\/[\w\-\+\.]+)?')
+MIMETYPE_REGEX_OBJ = re.compile(r'^' + MIMETYPE_REGEX_PATTERN + r'$')
+
+CHARSET_REGEX_PATTERN = (r'(\;charset\=(?P<charset>)[\w\-\+\.]+)?' )
+CHARSET_REGEX_OBJ = re.compile(r'^' + CHARSET_REGEX_PATTERN + r'$')
+
+DATA_URI_REGEX = (r'data:' + MIMETYPE_REGEX_PATTERN + CHARSET_REGEX_PATTERN + r'(\;(?P<base64>base64))?' + r',(?P<data>.*)')
 
 class DataURI(object):
 
@@ -27,6 +33,27 @@ class DataURI(object):
     def data(self):
         return self._data
 
+    @classmethod
+    def formatter(cls, mimetype='text/plain', charset='us-ascii', data=None):
+        pattern = 'data:'
+        if mimetype is not None:
+            if MIMETYPE_REGEX_OBJ.match(str(mimetype)) is None:
+                raise ValueError('Invalid mimetype: %r' % mimetype)
+            pattern = '%s%s' % (pattern, mimetype)
+
+        if charset is not None:
+            if CHARSET_REGEX_OBJ.match((';charset=%s' % charset)) is None:
+                raise ValueError('Invalid charset: %r' % charset)
+            pattern = '%s;charset=%s' % (pattern, charset)
+
+        if data is None:
+            raise ValueError('Data should not be none')
+
+        encoded_data = urlsafe_b64encode(data)
+        pattern = '%s;base64,%s' % (pattern, encoded_data)
+
+        return pattern
+
     def _parse(self, data_str):
         match = re.match(DATA_URI_REGEX, data_str)
         if not match:
@@ -39,4 +66,3 @@ class DataURI(object):
             raise ValueError('Resource is not base64 encoded: %r' % data_str)
 
         return parsed_data['mimetype'], parsed_data['charset'], decoded_data
-
